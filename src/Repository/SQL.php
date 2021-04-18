34b086bb8d4fd1d25712f2588055d23e2a4cb30d
Baptiste Langlade <baptouuuu@gmail.com>
2021-04-18 16:03:01 +0200

    move the sql logic outside the repository

--- src/Repository/SQL.php
+++ src/Repository/SQL.php
@@ -9,15 +9,11 @@ use Formal\ORM\{
     Definition\Aggregate,
     SQL\Types,
     SQL\MatchId,
+    SQL\Table,
     SQL\Table\Normalize,
     SQL\Table\Denormalize,
 };
-use Formal\AccessLayer\{
-    Connection,
-    Row,
-    Query,
-    Table,
-};
+use Formal\AccessLayer\Connection;
 use Innmind\Specification\Specification;
 use Innmind\Immutable\{
     Maybe,
@@ -34,12 +30,8 @@ final class SQL implements Repository
     private string $class;
     /** @var Aggregate<V> */
     private Aggregate $aggregate;
-    private Connection $connection;
-    private Types $types;
-    /** @var Normalize<V> */
-    private Normalize $normalize;
-    /** @var Denormalize<V> */
-    private Denormalize $denormalize;
+    /** @var Table<V> */
+    private Table $table;
     /** @var callable(Id<V>): Maybe<V> */
     private $lookup;
     /** @var callable(Id<V>, V): void */
@@ -69,10 +61,7 @@ final class SQL implements Repository
     ) {
         $this->class = $class;
         $this->aggregate = $aggregate;
-        $this->connection = $connection;
-        $this->types = $types;
-        $this->normalize = new Normalize($aggregate, $types);
-        $this->denormalize = new Denormalize($aggregate, $types);
+        $this->table = new Table($aggregate, $types, $connection);
         $this->lookup = $lookup;
         $this->cache = $cache;
         $this->invalidate = $invalidate;
@@ -82,7 +71,19 @@ final class SQL implements Repository
     public function get(Id $id): Maybe
     {
         return ($this->lookup)($id)
-            ->otherwise(fn() => $this->lookup($id));
+            ->otherwise(fn() => $this->table->get($id))
+            ->map(function($aggregate) {
+                /**
+                 * @psalm-suppress InvalidArgument Don't know why it loses the template here
+                 * @var V $aggregate
+                 */
+                ($this->cache)(
+                    $this->extractId($aggregate),
+                    $aggregate,
+                );
+
+                return $aggregate;
+            });
     }
 
     public function add(object $aggregate): void
@@ -92,8 +93,8 @@ final class SQL implements Repository
         $id = $this->extractId($aggregate);
         /** @psalm-suppress UnusedMethodCall */
         ($this->lookup)($id)->match(
-            fn() => $this->update($id, $aggregate),
-            fn() => $this->insert($aggregate),
+            fn() => $this->table->update($id, $aggregate),
+            fn() => $this->table->insert($aggregate),
         );
 
         ($this->cache)($id, $aggregate);
@@ -103,21 +104,13 @@ final class SQL implements Repository
     {
         $this->assertMutable();
 
-        ($this->connection)(
-            (new Query\Delete(new Table\Name($this->aggregate->name())))
-                ->where($this->match($id)),
-        );
+        $this->table->delete($id);
         ($this->invalidate)($id);
     }
 
     public function all(): Set
     {
-        return ($this->connection)($this->select())
-            ->mapTo(
-                $this->class,
-                fn($row) => ($this->denormalize)($row),
-            )
-            ->toSetOf($this->class);
+        return $this->table->all();
     }
 
     public function matching(Specification $specification): Set
@@ -135,45 +128,6 @@ final class SQL implements Repository
         }
     }
 
-    private function select(): Query\Select
-    {
-        return new Query\Select(new Table\Name($this->aggregate->name()));
-    }
-
-    private function match(Id $id): Specification
-    {
-        return new MatchId(
-            $this->aggregate->id()->property(),
-            $id,
-        );
-    }
-
-    /**
-     * @param Id<V> $id
-     *
-     * @return Maybe<V>
-     */
-    private function lookup(Id $id): Maybe
-    {
-        $select = $this->select()->where($this->match($id));
-        $aggregates = ($this->connection)($select)
-            ->mapTo($this->class, fn($row) => ($this->denormalize)($row));
-
-        if (!$aggregates->empty()) {
-            $aggregate = $aggregates->first();
-
-            ($this->cache)(
-                $this->extractId($aggregate),
-                $aggregate,
-            );
-
-            return Maybe::just($aggregate);
-        }
-
-        /** @var Maybe<V> */
-        return Maybe::nothing();
-    }
-
     /**
      * @param V $aggregate
      *
@@ -182,30 +136,5 @@ final class SQL implements Repository
     private function extractId(object $aggregate): Id
     {
         return $this->aggregate->id()->extract($aggregate);
-    }
-
-    /**
-     * @param V $aggregate
-     */
-    private function insert(object $aggregate): void
-    {
-        ($this->connection)(new Query\Insert(
-            new Table\Name($this->aggregate->name()),
-            Row::of(($this->normalize)($aggregate)),
-        ));
-    }
-
-    /**
-     * @param Id<V> $id
-     * @param V $aggregate
-     */
-    private function update(Id $id, object $aggregate): void
-    {
-        // todo only update what changed
-        $update = new Query\Update(
-            new Table\Name($this->aggregate->name()),
-            Row::of(($this->normalize)($aggregate)),
-        );
-        ($this->connection)($update->where($this->match($id)));
     }
 }
