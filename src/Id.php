7d03f6ece8414ca55a88920cab8a250e44b6f880
Baptiste Langlade <baptouuuu@gmail.com>
2023-07-14 09:26:53 +0200

    add Id implementation

--- src/Id.php
+++ src/Id.php
@@ -3,16 +3,24 @@ declare(strict_types = 1);
 
 namespace Formal\ORM;
 
+use Ramsey\Uuid\Uuid;
+
 /**
+ * @psalm-immutable
  * @template T of object
  */
 final class Id
 {
+    /** @var non-empty-string */
+    private string $value;
+
     /**
      * @param class-string<T> $class
+     * @param non-empty-string $value
      */
-    private function __construct(string $class)
+    private function __construct(string $class, string $value)
     {
+        $this->value = $value;
     }
 
     /**
@@ -24,6 +32,40 @@ final class Id
      */
     public static function new(string $class): self
     {
-        return new self($class);
+        return new self($class, Uuid::uuid4()->toString());
+    }
+
+    /**
+     * @template A of object
+     * @psalm-pure
+     *
+     * @param class-string<A> $class
+     * @param non-empty-string $value
+     *
+     * @return self<A>
+     */
+    public static function of(string $class, string $value): self
+    {
+        if (!Uuid::isValid($value)) {
+            throw new \LogicException("Invalid id value '$value'");
+        }
+
+        return new self($class, $value);
+    }
+
+    /**
+     * @param self<T> $other
+     */
+    public function equals(self $other): bool
+    {
+        return $this->value === $other->value;
+    }
+
+    /**
+     * @return non-empty-string
+     */
+    public function toString(): string
+    {
+        return $this->value;
     }
 }
