ae88d82f0c3531f0f38babfdc4f27f46275014dc
Baptiste Langlade <baptouuuu@gmail.com>
2023-08-13 17:01:22 +0200

    allow to auto-close sql connections when no longer used

--- src/Adapter/SQL.php
+++ src/Adapter/SQL.php
@@ -11,30 +11,61 @@ use Formal\AccessLayer\Connection;
 
 final class SQL implements Adapter
 {
-    private Connection $connection;
-    private SQL\Transaction $transaction;
+    /** @var callable(): Connection  */
+    private $establish;
+    /** @var \WeakReference<Connection> */
+    private \WeakReference $established;
 
-    private function __construct(Connection $connection)
+    /**
+     * @param callable(): Connection $establish
+     */
+    private function __construct(callable $establish)
     {
-        $this->connection = $connection;
-        $this->transaction = SQL\Transaction::of($connection);
+        $this->establish = $establish;
+        /**
+         * Using an unassigned object creates an empty WeakReference
+         * @var \WeakReference<Connection>
+         */
+        $this->established = \WeakReference::create(new \stdClass);
     }
 
     public static function of(Connection $connection): self
     {
-        return new self($connection);
+        return new self(static fn() => $connection);
+    }
+
+    /**
+     * @param callable(): Connection $establish
+     */
+    public static function lazy(callable $establish): self
+    {
+        return new self($establish);
     }
 
     public function repository(Aggregate $definition): Repository
     {
         return SQL\Repository::of(
-            $this->connection,
+            $this->connection(),
             $definition,
         );
     }
 
     public function transaction(): Transaction
     {
-        return $this->transaction;
+        return SQL\Transaction::of($this->connection());
+    }
+
+    private function connection(): Connection
+    {
+        $connection = $this->established->get();
+
+        if ($connection) {
+            return $connection;
+        }
+
+        $connection = ($this->establish)();
+        $this->established = \WeakReference::create($connection);
+
+        return $connection;
     }
 }
