2c7e63bf9a682dbbc3cbd8d2d5bbced0102a3979
Baptiste Langlade <baptouuuu@gmail.com>
2024-05-22 15:38:00 +0200

    fix psalm errors

--- src/Adapter/Elasticsearch/Decode.php
+++ src/Adapter/Elasticsearch/Decode.php
@@ -185,8 +185,7 @@ final class Decode
     private static function collection(Definition\Collection $collection): Constraint
     {
         /** @psalm-suppress MixedArgument Due to the array keys */
-        return Is::array()
-            ->and(Is::list())
+        return Is::list()
             ->and(Each::of(
                 self::properties($collection->properties())->map(
                     Aggregate\Collection\Entity::of(...),
